93e8512b533425f9c57ea0a0fce6ab3cc59a0b11
Jason <jiacan.h01@gmail.com>
2024-01-17 15:04:34 -0500

    Upgrade to reactive form

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import * as Parse from 'parse';
 
@@ -7,21 +8,17 @@ import * as Parse from 'parse';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit{
-  username: string = '';
-  password: string = '';
-  error: boolean = false;
+export class LoginPage {
+  form = new FormGroup({
+    username: new FormControl<string>('', [Validators.required]),
+    password: new FormControl<string>('', [Validators.required]),
+  });
 
   constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.username = '';
-    this.password = '';
-    this.error = false;
-  }
-
   logIn() {
-    Parse.User.logIn(this.username, this.password).then((result) => {
+    const data = this.form.getRawValue();
+    Parse.User.logIn(data.username as string, data.password as string).then((result) => {
       this.router.navigate(['/home']);
     }).catch((error) => {
       alert("Invalid credentials!");
@@ -29,23 +26,16 @@ export class LoginPage implements OnInit{
   }
 
   signUp() {
+    const data = this.form.getRawValue();
     const user = new Parse.User();
-    user.set('username', this.username);
-    user.set('password', this.password);
+    user.set('username', data.username);
+    user.set('password', data.password);
 
     user.signUp().then((result) => {
-      this.clear();
       this.router.navigate(['/home']);
     }).catch((error) => {
-      this.error = true;
       alert("Username is taken!");
     });
-  }
-
-  clear() {
-    this.username = '';
-    this.password = '';
-    this.error = false;
   }
 
 }
