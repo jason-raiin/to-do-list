842c98e66fc8017b092c8e2e899e172aa72400ac
Jason <jiacan.h01@gmail.com>
2024-01-17 11:11:21 -0500

    Implement auth functionality

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Parse from 'parse';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  username: string = '';
+  password: string = '';
+  error: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  logIn() {
+    Parse.User.logIn(this.username, this.password).then((result) => {
+      this.router.navigate(['/home']);
+    }).catch((error) => {
+      alert("Invalid credentials!");
+    });
+  }
+
+  async signUp() {
+    const user = new Parse.User();
+    user.set('username', this.username);
+    user.set('password', this.password);
+
+    user.signUp().then((result) => {
+      this.router.navigate(['/home']);
+    }).catch((error) => {
+      this.error = true;
+      alert("Username is taken!");
+    });
   }
 
 }
