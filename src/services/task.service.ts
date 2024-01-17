d0b6e5d15b985ef38c548e6a31b116b491e17bd5
Jason <jiacan.h01@gmail.com>
2024-01-17 15:45:18 -0500

    Create task getters and setters

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -2,10 +2,41 @@ import * as Parse from 'parse';
 
 export class Task extends Parse.Object{
   
-  constructor(){
+  constructor() {
     super('Task');
+    this.set('complete', false);
   }
 
+  static create(title: string, date: number) {
+    const task = new Task();
+    task.title = title;
+    task.date = date;
+  }
+
+  get title() {
+    return this.get('title');
+  }
+
+  set title(title: string) {
+    this.set('title', title);
+  }
+
+  get date() {
+    return this.get('date');
+  }
+
+  set date(date: number) {
+    this.set('date', date);
+  }
+
+  get complete() {
+    return this.get('complete');
+  }
+
+  toggleComplete() {
+    this.set('complete', !this.complete());
+    return this.complete;
+  }
 
 }
 
