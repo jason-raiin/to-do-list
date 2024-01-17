0a11bd379eb534ae387849d0641fe6166c725480
Jason <jiacan.h01@gmail.com>
2024-01-17 15:51:50 -0500

    Add default task list

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -11,6 +11,15 @@ export class Task extends Parse.Object{
     const task = new Task();
     task.title = title;
     task.date = date;
+    return task;
+  }
+
+  static defaultTasks() {
+    return [
+        Task.create('Pay the bills', 1705539600),
+        Task.create('Wash the dishes', 1705341600),
+        Task.create('Feed the dog', 1705413600),
+    ];
   }
 
   get title() {
