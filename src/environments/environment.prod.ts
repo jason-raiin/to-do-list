f092f51f14358f649a7194e5c53482528e649911
Jason <jiacan.h01@gmail.com>
2024-01-26 12:09:43 -0500

    Update prod server IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PARSE_SERVER_URL: "http://18.216.222.244/parse",
+  PARSE_SERVER_URL: "http://3.15.12.72/parse",
   PARSE_APP_ID: "todo",
   PARSE_MASTER_KEY: "supersecret",
 };
