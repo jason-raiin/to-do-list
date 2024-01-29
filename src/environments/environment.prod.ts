630a860d7a3f6a317c2a97711b3aae12debc6533
Jason <jiacan.h01@gmail.com>
2024-01-29 15:02:56 -0500

    Update server URI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PARSE_SERVER_URL: "http://3.15.12.72/parse",
+  PARSE_SERVER_URL: "http://serverloadbalancer-1331232252.us-east-1.elb.amazonaws.com/parse",
   PARSE_APP_ID: "todo",
   PARSE_MASTER_KEY: "supersecret",
 };
